b87882a7e5ee341ae5bc4f55836364d2e4eb5299
Chris McLeod <chris@chrismcleod.dev>
2024-05-31 22:17:41 +0100

    feature: rewrite dingus2 to use ChromaDB, refactoring

--- dingus2.py
+++ dingus2.py
@@ -1,113 +1,58 @@
 import argparse
 import glob
+import json
 import os
-import pickle
 import re
+import shutil
+import time
 
-import numpy as np
-import torch
-from bs4 import BeautifulSoup
-from docx import Document
+
 from dotenv import load_dotenv
 from openai import OpenAI
 import ollama
-import time
-'''
- This version of Dingus (v2) is a smooshing together of ideas from Dingus v1,
- and code by All About AI:
- - Video: https://www.youtube.com/watch?v=Oe-7dGDyzPM
- - Repo: https://github.com/AllAboutAI-YT/easy-local-rag
 
-'''
+from util import (
+    format_elapsed_time,
+    extract_text,
+    AnsiColours as ansi,
+    get_database,
+    load_or_generate,
+    rewrite_query,
+)
+
+"""
+ This version of Dingus (v2) is a smooshing together of ideas from Dingus v1,
+ code by All About AI, and an example from Ollama:
+ - All About AI Video: https://www.youtube.com/watch?v=Oe-7dGDyzPM
+ - All About AI Repo: https://github.com/AllAboutAI-YT/easy-local-rag
+ - Ollama Example: https://ollama.com/blog/embedding-models
+
+"""
 
 # Load environment variables from .env file
 load_dotenv()
 
-DOCUMENTS_FILE = os.environ.get('DOCUMENTS_FILE',"documents2.pkl")
-EMBEDDINGS_FILE = os.environ.get('EMBEDDINGS_FILE',"embeddings2.npy")
-OLLAMA_MODEL = os.environ.get('OLLAMA_MODEL','phi3:mini')
-EMBEDDING_MODEL = os.environ.get('EMBEDDING_MODEL','mxbai-embed-large')
-LOCAL_DOCUMENTS = os.environ.get('DOCS_LOCATION', '')
-OPENAI_API_BASE = os.environ.get('OPENAI_BASE_URL', 'http://localhost:11434/v1')
+DOCUMENTS_FILE = os.environ.get("DOCUMENTS_FILE", "documents2.pkl")
+EMBEDDINGS_FILE = os.environ.get("EMBEDDINGS_FILE", "embeddings2.npy")
+OLLAMA_MODEL = os.environ.get("OLLAMA_MODEL", "phi3:mini")
+EMBEDDING_MODEL = os.environ.get("EMBEDDING_MODEL", "mxbai-embed-large")
+LOCAL_DOCUMENTS = os.environ.get("DOCS_LOCATION", "./docs/**/*.txt")
+OPENAI_API_BASE = os.environ.get("OPENAI_BASE_URL", "http://localhost:11434/v1")
+CHROMADB_FILE = os.environ.get("CHROMADB_FILE", "chromadb.db")
+RELEVANT_RESULTS = int(os.environ.get("RELEVANT_RESULTS", 3))
 
-client = OpenAI(
-    base_url=OPENAI_API_BASE,
-    api_key=OLLAMA_MODEL
-)
+print(" ::: Environment :::")
+print(f"DOCUMENTS_FILE: {DOCUMENTS_FILE}")
+print(f"EMBEDDINGS_FILE: {EMBEDDINGS_FILE}")
+print(f"OLLAMA_MODEL: {OLLAMA_MODEL}")
+print(f"EMBEDDING_MODEL: {EMBEDDING_MODEL}")
+print(f"LOCAL_DOCUMENTS: {LOCAL_DOCUMENTS}")
+print(f"OPENAI_API_BASE: {OPENAI_API_BASE}")
+print(f"CHROMADB_FILE: {CHROMADB_FILE}")
+print(f"RELEVANT_RESULTS: {RELEVANT_RESULTS}")
 
-# File extraction functions
-def extract_text_from_html(file_path):
-    """
-    Extracts text content from an HTML file.
-
-    Args:
-        file_path (str): The path to the HTML file.
-
-    Returns:
-        str: The extracted text content from the HTML file.
-    """
-    with open(file_path, 'r', encoding='utf-8') as file:
-        soup = BeautifulSoup(file, 'html.parser')
-        return soup.get_text(strip=True)
-
-def extract_text_from_docx(file_path):
-    """
-    Extracts the text content from a DOCX file.
-
-    Args:
-        file_path (str): The path to the DOCX file.
-
-    Returns:
-        str: The extracted text content.
-    """
-    doc = Document(file_path)
-    text = ""
-    for para in doc.paragraphs:
-        text += para.text
-    return text
-
-def extract_text_from_txt(file_path):
-    """
-    Extracts the text content from a text file.
-
-    Args:
-        file_path (str): The path to the text file.
-
-    Returns:
-        str: The content of the text file.
-
-    Raises:
-        FileNotFoundError: If the file does not exist.
-        IOError: If there is an error reading the file.
-
-    """
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return file.read()
-
-def extract_text(file_path):
-    """
-    Extracts text from a given file.
-
-    Args:
-        file_path (str): The path to the file.
-
-    Returns:
-        str: The extracted text from the file.
-
-    Raises:
-        None
-
-    """
-    extractors = {
-        ".html": extract_text_from_html,
-        ".docx": extract_text_from_docx,
-        ".txt": extract_text_from_txt,
-        ".md": extract_text_from_txt
-    }
-    ext = os.path.splitext(file_path)[1].lower()
-    extractor = extractors.get(ext)
-    if extractor:
-        return extractor(file_path)
+api = OpenAI(base_url=OPENAI_API_BASE, api_key=OLLAMA_MODEL)
+db, collection = None, None
 
 
 def chunk_text(text):
@@ -121,9 +66,11 @@ def chunk_text(text):
         list: A list of chunks, where each chunk is a string of sentences separated by two newlines.
     """
     # Normalize whitespace and clean up text
-    text = re.sub(r'\s+', ' ', text).strip()
+    text = re.sub(r"\s+", " ", text).strip()
     # Split text into chunks by sentences, respecting a maximum chunk size
-    sentences = re.split(r'(?<=[.!?]) +', text)  # split on spaces following sentence-ending punctuation
+    sentences = re.split(
+        r"(?<=[.!?]) +", text
+    )  # split on spaces following sentence-ending punctuation
     chunks = []
     current_chunk = ""
     for sentence in sentences:
@@ -136,196 +83,178 @@ def chunk_text(text):
             current_chunk = sentence + " "
     if current_chunk:  # Don't forget the last chunk!
         chunks.append(current_chunk)
-    # return a string of all chunks separated by 2 newlines
     return chunks
 
-def load_documents():
-    """
-    Load documents from disk or pickle file.
 
-    If the pickle file exists, it loads the documents from the file.
-    Otherwise, it loads the documents from the disk by extracting text from the specified file paths,
-    chunking the text, and appending the chunks to the documents list.
+def generate_documents():
+    """
+    Generate a list of pre-processed documents from the specified file paths.
 
     Returns:
-        List: The list of loaded document fragments.
+        list: A list of pre-processed document fragments.
+
+    Raises:
+        ValueError: If an error occurs during text extraction.
     """
-    if os.path.exists(DOCUMENTS_FILE):
-        with open(DOCUMENTS_FILE, 'rb') as f:
-            documents = pickle.load(f)
+    print("No pre-processed documents found. Loading from disk...")
+    documents = []
+    file_paths = []
+    if "," in LOCAL_DOCUMENTS:
+        segments = LOCAL_DOCUMENTS.split(",")
+        for segment in segments:
+            file_paths.extend(glob.glob(segment.strip(), recursive=True))
     else:
-        documents = []
-    # if no documents, load from disk
-    if not documents:
-        print("No pre-processed documents found. Loading from disk...")
-        file_paths = []
-        if ',' in LOCAL_DOCUMENTS:
-            segments = LOCAL_DOCUMENTS.split(',')
-            for segment in segments:
-                file_paths.extend(glob.glob(segment.strip(), recursive=True))
-        else:
-            file_paths = glob.glob(LOCAL_DOCUMENTS, recursive=True)
-        for file_path in file_paths:
-            try:
-                text = extract_text(file_path)
-                if text:
-                    chunks = chunk_text(text)
-                    # append all chunks to documents
-                    documents.extend(chunks)
-            except ValueError as e:
-                print(e)
-        print(f"Processed {len(documents)} document fragments.")
-        #pickle documents
-        with open(DOCUMENTS_FILE, 'wb') as f:
-            print("Pickling documents...")
-            pickle.dump(documents, f)
+        file_paths = glob.glob(LOCAL_DOCUMENTS, recursive=True)
+    for file_path in file_paths:
+        try:
+            text = extract_text(file_path)
+            if text:
+                chunks = chunk_text(text)
+                # append all chunks to documents
+                documents.extend(chunks)
+        except ValueError as e:
+            print(e)
+    print(f"Processed {len(documents)} document fragments.")
+    # remove any empty documents
+    documents = [doc for doc in documents if doc]
     return documents
 
-def generate_embeddings(documents):
-    """
-    Generate embeddings for a list of documents.
 
-    If the embeddings file already exists, it will be loaded.
-    Otherwise, embeddings will be generated using the specified embedding model and saved to the file.
+def generate_embeddings_chromadb(documents, collection):
+    """
+    Generate embeddings for a list of documents and store them in a vector embedding database.
 
     Args:
-        documents (list): A list of document fragments.
+        documents (list): A list of documents to generate embeddings for.
+        collection: The vector embedding database collection to store the embeddings in.
 
     Returns:
-        numpy.ndarray: An array of embeddings corresponding to the input documents.
+        None
     """
-    if os.path.exists(EMBEDDINGS_FILE):
-        print("Loading embeddings...")
-        with open(EMBEDDINGS_FILE, 'rb') as f:
-            embeddings = np.load(f)
-    else:
-        print("Generating embeddings... please be patient, as it can take a long time...")
-        '''
-        As a point of reference: generating embeddings for 2154 document fragments took about
-        3 hours on a Raspberry Pi 5 with 8GB of RAM and an SSD, when using mxbai-embed-large
-        '''
-        embeddings = []
-        start_time = time.time()
-        for content in documents:
-            response = ollama.embeddings(model=EMBEDDING_MODEL, prompt=content)
-            embeddings.append(response["embedding"])
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        if elapsed_time > 60:
-            minutes = int(elapsed_time // 60)
-            seconds = int(elapsed_time % 60)
-            if minutes > 60:
-                hours = int(minutes // 60)
-                minutes = int(minutes % 60)
-                print(f"Embeddings generated in {hours}h {minutes}m {seconds}s.")
-            else:
-                print(f"Embeddings generated in {minutes}m {seconds}s.")
-        else:
-            print(f"Embeddings generated in {elapsed_time}s.")
-        print("Saving embeddings...")
-        with open(EMBEDDINGS_FILE, 'wb') as f:
-            np.save(f, embeddings)
-    return embeddings
+    start_time = time.time()
+    for i, d in enumerate(documents):
+        response = ollama.embeddings(model=EMBEDDING_MODEL, prompt=d)
+        embedding = response["embedding"]
+        collection.add(ids=[str(i)], embeddings=[embedding], documents=[d])
+    end_time = time.time()
+    elapsed_time = format_elapsed_time((end_time - start_time))
+    print(f"Embeddings generated in {elapsed_time}.")
 
-# Function to get relevant context from the vault based on user input
-def get_relevant_context(input, embeddings, content, top_k=3):
-    if embeddings.nelement() == 0:  # Check if the tensor has any elements
-        return []
-    # Encode the rewritten input
-    input_embedding = ollama.embeddings(model=EMBEDDING_MODEL, prompt=input)["embedding"]
-    # Compute cosine similarity between the input and vault embeddings
-    cos_scores = torch.cosine_similarity(torch.tensor(input_embedding).unsqueeze(0), embeddings)
-    # Adjust top_k if it's greater than the number of available scores
-    top_k = min(top_k, len(cos_scores))
-    # Sort the scores and get the top-k indices
-    top_indices = torch.topk(cos_scores, k=top_k)[1].tolist()
-    # Get the corresponding context from the vault
-    relevant_context = [content[idx].strip() for idx in top_indices]
-    return relevant_context
 
-def ollama_chat(user_input, system_message, embeddings, content, ollama_model, conversation_history):
+def get_relevant_context_chromadb(query, collection):
     """
-    Conducts a chat conversation with the Ollama model.
+    Retrieves the most relevant document from a collection based on a query.
 
     Args:
-        user_input (str): The user's input message.
-        system_message (str): The system's message.
-        embeddings: The embeddings used for retrieving relevant context.
-        content: The content used for retrieving relevant context.
-        ollama_model: The Ollama model used for generating responses.
-        conversation_history (list): The list of previous conversation messages.
+        query (str): The query string used to generate an embedding for the prompt.
+        collection (Collection): The collection object used to query the database.
 
     Returns:
-        str: The assistant's response.
+        list: A list of the most relevant documents retrieved from the collection.
+    """
+    # generate an embedding for the prompt and retrieve the most relevant doc
+    response = ollama.embeddings(prompt=query, model=EMBEDDING_MODEL)
+    results = collection.query(
+        query_embeddings=[response["embedding"]], n_results=RELEVANT_RESULTS
+    )
+    return [result["documents"][0] for result in results]
 
+
+def generate_answer(user_input, conversation_history, system_message, collection):
+    """
+    Generates an answer based on the user input, conversation history, system message, and collection.
+
+    Args:
+        user_input (str): The user's input.
+        conversation_history (list): A list of dictionaries representing the conversation history.
+        system_message (str): The system message.
+        collection (str): The collection.
+
+    Returns:
+        str: The generated answer.
     """
     conversation_history.append({"role": "user", "content": user_input})
-    
-    # if len(conversation_history) > 1:
-    #     query_json = {
-    #         "Query": user_input,
-    #         "Rewritten Query": ""
-    #     }
-    #     rewritten_query_json = rewrite_query(json.dumps(query_json), conversation_history, ollama_model)
-    #     rewritten_query_data = json.loads(rewritten_query_json)
-    #     rewritten_query = rewritten_query_data["Rewritten Query"]
-    #     print(PINK + "Original Query: " + user_input + RESET_COLOR)
-    #     print(PINK + "Rewritten Query: " + rewritten_query + RESET_COLOR)
-    # else:
-    rewritten_query = user_input
-    
-    relevant_context = get_relevant_context(rewritten_query, embeddings, content)
+
+    if len(conversation_history) > 1:
+        query_json = {"Query": user_input, "Rewritten Query": ""}
+        rewritten_query_json = rewrite_query(
+            json.dumps(query_json), conversation_history, api, OLLAMA_MODEL
+        )
+        rewritten_query_data = json.loads(rewritten_query_json)
+        rewritten_query = rewritten_query_data["Rewritten Query"]
+        print(ansi.PINK + "Original Query: " + user_input + ansi.RESET_COLOR)
+        print(ansi.PINK + "Rewritten Query: " + rewritten_query + ansi.RESET_COLOR)
+    else:
+        rewritten_query = user_input
+
+    relevant_context = get_relevant_context_chromadb(rewritten_query, collection)
+    context_str = ""
     if relevant_context:
         context_str = "\n".join(relevant_context)
-        print("Context Pulled from Documents: \n\n" + CYAN + context_str + RESET_COLOR)
+        print(
+            "Context Pulled from Documents: \n\n"
+            + ansi.CYAN
+            + context_str
+            + ansi.RESET_COLOR
+        )
     else:
-        print(CYAN + "No relevant context found." + RESET_COLOR)
-    
+        print(ansi.CYAN + "No relevant context found." + ansi.RESET_COLOR)
+
     user_input_with_context = user_input
     if relevant_context:
         user_input_with_context = user_input + "\n\nRelevant Context:\n" + context_str
-    
+
     conversation_history[-1]["content"] = user_input_with_context
-    
-    messages = [
-        {"role": "system", "content": system_message},
-        *conversation_history
-    ]
-    
-    response = client.chat.completions.create(
-        model=ollama_model,
+
+    messages = [{"role": "system", "content": system_message}, *conversation_history]
+
+    response = api.chat.completions.create(
+        model=OLLAMA_MODEL,
         messages=messages,
         max_tokens=2000,
     )
-    
-    conversation_history.append({"role": "assistant", "content": response.choices[0].message.content})
-    
+
+    conversation_history.append(
+        {"role": "assistant", "content": response.choices[0].message.content}
+    )
+
     return response.choices[0].message.content
+
 
 # Main script
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Local RAG Experiment")
-    parser.add_argument("--regen", action='store_true', help="Whether to regenerate the embeddings or not")
+    parser.add_argument(
+        "--regen",
+        action="store_true",
+        help="Whether to regenerate the embeddings or not",
+    )
     args = parser.parse_args()
 
-    #if args.regen is present then delete DOCUMENTS_FILE and EMBEDDINGS_FILE
+    # if args.regen is present then delete DOCUMENTS_FILE and EMBEDDINGS_FILE
     if args.regen is True:
+        # remove CHROMADB_FILE then recreate
+        if os.path.exists(CHROMADB_FILE):
+            shutil.rmtree(CHROMADB_FILE)
+        # remove DOCUMENTS_FILE
         if os.path.exists(DOCUMENTS_FILE):
             os.remove(DOCUMENTS_FILE)
-        if os.path.exists(EMBEDDINGS_FILE):
-            os.remove(EMBEDDINGS_FILE)
+
+    db, collection = get_database(CHROMADB_FILE)
 
     print("Getting documents...")
-    documents = load_documents()
+    documents = load_or_generate(DOCUMENTS_FILE, generate_documents, [])
     print("Getting embeddings...")
-    embeddings = generate_embeddings(documents)
+    # embeddings = generate_embeddings(documents)
 
-    # Convert to tensor and print embeddings
-    print("Converting embeddings to tensor...")
-    embeddings_tensor = torch.tensor(embeddings) 
-    print("Embeddings for each document fragment:")
-    print(embeddings_tensor)
+    # # Convert to tensor and print embeddings
+    # print("Converting embeddings to tensor...")
+    # embeddings_tensor = torch.tensor(embeddings)
+    # print("Embeddings for each document fragment:")
+    # print(embeddings_tensor)
+
+    generate_embeddings_chromadb(documents, collection)
 
     system_message = """
 You are a helpful assistant for question-answering tasks for a single user. Use the supplied context to answer the question. All the context is relevant to the user's interests and work. Use the context to answer the question as best as you can.
@@ -339,12 +268,15 @@ If you do know the answer, keep the answer concise. Bullet points and numbered l
         while True:
             query = input("\n\n\033[94mAsk Dingus² 🤖 ::::: \033[00m")
             # if query is bye then exit
-            if query.lower() == 'bye':
+            if query.lower() == "bye":
                 print("Exiting...")
                 break
-            
+
             # Retrieve and print the answer
-            answer = ollama_chat(query, system_message, embeddings_tensor, documents, OLLAMA_MODEL, conversation_history)
+            # answer = ollama_chat(query, system_message, embeddings_tensor, documents, OLLAMA_MODEL, conversation_history)
+            answer = generate_answer(
+                query, conversation_history, system_message, collectioni
+            )
             if answer is not None:
                 print(answer)
 
---
+++ util.py
@@ -0,0 +1,186 @@
+import json
+import pickle
+from bs4 import BeautifulSoup
+import chromadb
+from docx import Document
+import os
+
+
+# Constants
+# ANSI escape codes for colours
+class AnsiColours:
+    NEON_GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    BLUE = "\033[94m"
+    PINK = "\033[95m"
+    CYAN = "\033[96m"
+    RED = "\033[91m"
+    WHITE = "\033[97m"
+    RESET_COLOR = "\033[0m"
+
+
+def format_elapsed_time(elapsed_time):
+    """
+    Formats the elapsed time into a human-readable string.
+
+    Args:
+        elapsed_time (float): The elapsed time in seconds.
+
+    Returns:
+        str: The formatted elapsed time string.
+
+    """
+    if elapsed_time > 60:
+        minutes = int(elapsed_time // 60)
+        seconds = int(elapsed_time % 60)
+        if minutes > 60:
+            hours = int(minutes // 60)
+            minutes = int(minutes % 60)
+            output = f"{hours}h {minutes}m {seconds}s"
+        else:
+            output = f"{minutes}m {seconds}s"
+    else:
+        output = f"{elapsed_time}s"
+    return output
+
+
+# File extraction functions
+def extract_text_from_html(file_path):
+    """
+    Extracts text content from an HTML file.
+
+    Args:
+        file_path (str): The path to the HTML file.
+
+    Returns:
+        str: The extracted text content from the HTML file.
+    """
+    with open(file_path, "r", encoding="utf-8") as file:
+        soup = BeautifulSoup(file, "html.parser")
+        return soup.get_text(strip=True)
+
+
+def extract_text_from_docx(file_path):
+    """
+    Extracts the text content from a DOCX file.
+
+    Args:
+        file_path (str): The path to the DOCX file.
+
+    Returns:
+        str: The extracted text content.
+    """
+    doc = Document(file_path)
+    text = ""
+    for para in doc.paragraphs:
+        text += para.text
+    return text
+
+
+def extract_text_from_txt(file_path):
+    """
+    Extracts the text content from a text file.
+
+    Args:
+        file_path (str): The path to the text file.
+
+    Returns:
+        str: The content of the text file.
+
+    Raises:
+        FileNotFoundError: If the file does not exist.
+        IOError: If there is an error reading the file.
+
+    """
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
+
+
+def extract_text(file_path):
+    """
+    Extracts text from a given file.
+
+    Args:
+        file_path (str): The path to the file.
+
+    Returns:
+        str: The extracted text from the file.
+
+    Raises:
+        None
+
+    """
+    extractors = {
+        ".html": extract_text_from_html,
+        ".docx": extract_text_from_docx,
+        ".txt": extract_text_from_txt,
+        ".md": extract_text_from_txt,
+    }
+    ext = os.path.splitext(file_path)[1].lower()
+    extractor = extractors.get(ext)
+    if extractor:
+        return extractor(file_path)
+
+
+def load_or_generate(filename, generate_function, default=None):
+    """
+    Load a file if it exists, otherwise generate it.
+
+    Args:
+        filename (str): The name of the file.
+        generate_function (function): The function to generate the file.
+
+    Returns:
+        object: The loaded or generated object.
+
+    """
+    if os.path.exists(filename):
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    else:
+        obj = generate_function()
+        with open(filename, "wb") as f:
+            pickle.dump(obj, f)
+        # Return the default value if the object is None
+        if obj is None:
+            return default
+        return obj
+
+
+def rewrite_query(user_input_json, conversation_history, client, ollama_model):
+    user_input = json.loads(user_input_json)["Query"]
+    context = "\n".join(
+        [f"{msg['role']}: {msg['content']}" for msg in conversation_history[-2:]]
+    )
+    prompt = f"""Rewrite the following query by incorporating relevant context from the conversation history.
+    The rewritten query should:
+    
+    - Preserve the core intent and meaning of the original query
+    - Expand and clarify the query to make it more specific and informative for retrieving relevant context
+    - Avoid introducing new topics or queries that deviate from the original query
+    - DONT EVER ANSWER the Original query, but instead focus on rephrasing and expanding it into a new query
+    
+    Return ONLY the rewritten query text, without any additional formatting or explanations.
+    
+    Conversation History:
+    {context}
+    
+    Original query: [{user_input}]
+    
+    Rewritten query: 
+    """
+    response = client.chat.completions.create(
+        model=ollama_model,
+        messages=[{"role": "system", "content": prompt}],
+        max_tokens=200,
+        n=1,
+        temperature=0.1,
+    )
+    rewritten_query = response.choices[0].message.content.strip()
+    return json.dumps({"Rewritten Query": rewritten_query})
+
+
+def get_database(db_file):
+    db = chromadb.PersistentClient(path=db_file)
+    collection = db.create_collection(name="docs")
+    return db, collection
